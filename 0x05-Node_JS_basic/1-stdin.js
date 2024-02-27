7464bdab38e8632493d895020df7c2da01ee2426
Harry Muriithi <hharryjjack@gmail.com>
2024-02-27 12:46:40 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
 // program that will be executed through command line
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log("This important software is now closing");
-  rl.close();
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
