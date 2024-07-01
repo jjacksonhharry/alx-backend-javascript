1ccd446694a0341d6cf467487d17bb1800848e7a
Harry Muriithi <hharryjjack@gmail.com>
2024-07-01 21:33:22 +0300

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 // 0-promise.js
-export default function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
     resolve('Success');
   });
 }
