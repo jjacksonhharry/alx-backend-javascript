fff7a37d244f0af25aa858228c92d04de103dd5b
Harry Muriithi <hharryjjack@gmail.com>
2024-07-01 20:54:52 +0300

    Await / Async

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+// 2-then.js
+export default function handleResponseFromAPI (promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success'
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
